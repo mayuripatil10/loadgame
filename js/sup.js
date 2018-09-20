41a39a593907211df9b376f90651cbe51a49a3e5
mayuripatil10 <you@example.com>
2018-09-20 13:59:25 +0530

    first commit

---
+++ js/sup.js
@@ -0,0 +1,10 @@
+var demoContainer=document.getElementById ("demo");
+var btn=document.getElementById ("btn");
+
+
+btn.addEventListener ("click",function() {
+
+    $('#demo').load("index.html");
+
+
+});
